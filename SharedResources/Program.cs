4e00c72426f882c4601fb99349cf559ea43a5695
Omkar <bhagwat@imagine-computation.com>
2020-11-07 10:53:44 +0100

    add threading realted projects

---
+++ SharedResources/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SharedResources
+{
+    class Program
+    {
+        private static bool isCompleted;
+        static readonly object LockCompleted = new object();
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(HellowWorld);
+            thread.Start();
+
+            HellowWorld();
+
+            Console.ReadLine();
+        }
+
+        private static void HellowWorld()
+        {
+            lock (LockCompleted)
+            {
+                if (!isCompleted)
+                {
+                    Console.WriteLine("Hello World should be printed only once.");
+                    isCompleted = true;
+                }
+            }
+
+
+        }
+    }
+}
--- Threading/Program.cs
+++ Threading/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Threading
@@ -10,6 +11,26 @@ namespace Threading
     {
         static void Main(string[] args)
         {
+            //create a thread
+            Thread thread = new Thread(MySampleThread);
+            thread.Name = "my worker thread";
+            thread.Start();
+
+            Thread.CurrentThread.Name = "Main IC Thread";
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Z" + "_" + i);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void MySampleThread()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("A" + "_" + i);
+            }
         }
     }
 }
