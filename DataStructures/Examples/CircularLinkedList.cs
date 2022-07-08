1ac01a4ec28d41b3fd1c1974f7198da9ad8974f1
Omkar Bhagwat <omkar.bhagwat@aeditive.de>
2022-07-08 08:52:00 +0200

    Tower Hanoi initiated

---
+++ DataStructures/Examples/CircularLinkedList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Examples
+{
+    public class CircularLinkedList<T> : LinkedList<T>
+    {
+        public new IEnumerator GetEnumerator()
+        {
+            return new CircularLinkedListEnumerator<T>(this);
+        }
+    }
+
+
+    public class CircularLinkedListEnumerator<T> : IEnumerator<T>
+    {
+        LinkedListNode<T> _current;
+        public T Current => _current.Value;
+
+        object IEnumerator.Current => Current;
+
+
+        public CircularLinkedListEnumerator(LinkedList<T> list)
+        {
+            _current = list.First;
+        }
+
+        public void Dispose()
+        {
+           
+        }
+
+        public bool MoveNext()
+        {
+            if(_current == null)
+                return false;
+
+            _current = _current.Next ?? _current.List.First;
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            _current = _current.List.First;
+        }
+    }
+
+    public static class CircularLinkedListExtension
+    {
+        public static LinkedListNode<T> Next<T>(this LinkedListNode<T> node)
+        {
+            if (node != null && node.List != null)
+            {
+                return node.Next ?? node.List.First;
+            }
+
+            return null;
+        }
+
+        public static LinkedListNode<T> Prvious<T>(this LinkedListNode<T> node)
+        {
+            if (node != null && node.List != null)
+            {
+                return node.Previous ?? node.List.Last;
+            }
+
+            return null;
+        }
+           
+    }
+}
---
+++ DataStructures/Examples/TowerHanoi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Examples
+{
+    public class TowerHanoi
+    {
+        public int DiscCount { get; private set; }
+
+        public int MovesCount { get; private set; }
+
+        public Stack<int> From { get; private set; }
+        public Stack<int> To { get; private set; }
+
+        public Stack<int> Auxiliary { get; private set; }
+
+        public event EventHandler<EventArgs> MoveCompleted;
+
+        public TowerHanoi(int discCount)
+        {
+            this.DiscCount = discCount;
+            this.From = new Stack<int>();
+            this.To = new Stack<int>();
+            this.Auxiliary = new Stack<int>();
+
+            //largest size block goes to bottom
+            for (int i = discCount; i > 0; i--)
+            {
+                this.From.Push(i);
+            }
+        }
+
+        public void Start()
+        {
+            
+        }
+
+        public void Move(int discs, Stack<int> from, Stack<int> to,
+            Stack<int> auxiliary)
+        {
+            if (discs == 0)
+                return;
+
+
+        }
+    }
+}
